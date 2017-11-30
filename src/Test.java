ee2fa0aefbe5376e3be52fff6a5655b30d7e9240
ShadowsCreature <joaopaulomartinspro@gmail.com>
2017-11-30 00:44:04 -0100

    commented the stuff I did so you guys can see

--- src/Test.java
+++ src/Test.java
@@ -17,7 +17,11 @@ public class Test {
     public static void main(String[] args) throws InterruptedException {
 
 
-        Picture img = Grids.generateGrid("initial");
+        Picture img = Grids.generateGrid("initial");  // initial --> initial screen
+
+        //intro
+        //gamescreen
+        //credits              <-- use this four keywords when creating the screens like the example above ;)
 
         img.draw();
         Player cenas = new Player();
